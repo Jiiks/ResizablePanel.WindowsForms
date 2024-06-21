3d984848b53e4596215cdfed20fb0b11c2577aad
Alexei Stukov <stukov.alexei.va@gmail.com>
2024-06-21 12:33:27 +0300

    add resize flags

--- ResizablePanel.cs
+++ ResizablePanel.cs
@@ -1,19 +1,8 @@
 using System.ComponentModel;
-using System.Windows.Forms;
 
 namespace Jiiks.WinForms.Controls;
-internal class ResizablePanel : Panel {
 
-    [Description("Resize border size")]
-    public int ResizeBorderSize { get; set; } = 10;
-    [Description("Default Cursor")]
-    public Cursor NormalCursor { get; set; } = Cursors.Default;
-    [Description("Border Colour")]
-    public Color BorderColour { get; set; } = Color.Black;
-    [Description("Border Style")]
-    public ButtonBorderStyle RBorderStyle { get; set; } = ButtonBorderStyle.Solid;
-    [Description("Border Width")]
-    public int BorderWidth { get; set; } = 1;
+internal partial class ResizablePanel : Panel {
 
     public void GlobalMouseMove(MouseEventArgs e) { }
 
@@ -55,31 +44,39 @@ internal class ResizablePanel : Panel {
             var ptc = PointToClient(Cursor.Position);
             switch (_resizeMode) {
                 case ResizeMode.ResizeTopLeft:
+                    if (!TopLeftResize) return;
                     newLoc = new Point(ptc.X + Location.X, ptc.Y + Location.Y);
                     newSize = new Size(_startSize.Width - newLoc.X + _startPoint.X, _startSize.Height - newLoc.Y + _startPoint.Y);
                     break;
                 case ResizeMode.ResizeTop:
+                    if (!TopResize) return;
                     newLoc = new Point(Location.X, ptc.Y + Location.Y);
                     newSize = new Size(Width, _startSize.Height - newLoc.Y + _startPoint.Y);
                     break;
                 case ResizeMode.ResizeTopRight:
+                    if (!TopRightResize) return;
                     newLoc = new Point(Location.X, ptc.Y + Location.Y);
                     newSize = new Size(ptc.X, _startSize.Height - newLoc.Y + _startPoint.Y);
                     break;
                 case ResizeMode.ResizeRight:
+                    if (!RightResize) return;
                     newSize = new Size(ptc.X, Height);
                     break;
                 case ResizeMode.ResizeBottomRight:
+                    if (!BottomRightResize) return;
                     newSize = new Size(ptc.X, ptc.Y);
                     break;
                 case ResizeMode.ResizeBottom:
+                    if (!BottomResize) return;
                     newSize = new Size(Width, ptc.Y);
                     break;
                 case ResizeMode.ResizeBottomLeft:
+                    if (!BottomLeftResize) return;
                     newLoc = new Point(ptc.X + Location.X, Location.Y);
                     newSize = new Size(_startSize.Width - newLoc.X + _startPoint.X, ptc.Y);
                     break;
                 case ResizeMode.ResizeLeft:
+                    if (!LeftResize) return;
                     newLoc = new Point(ptc.X + Location.X, Location.Y);
                     newSize = new Size(_startSize.Width - newLoc.X + _startPoint.X, Height);
                     break;
@@ -100,58 +97,60 @@ internal class ResizablePanel : Panel {
 
     private void ChangeCursor(MouseEventArgs e) {
 
-        if(e.Location.X < ResizeBorderSize) {
+        if (e.Location.X < ResizeBorderSize) {
             // Top left corner
-            if (e.Location.Y < ResizeBorderSize) {
+            if (TopLeftResize && e.Location.Y < ResizeBorderSize) {
                 Cursor = Cursors.SizeNWSE;
                 _resizeMode = ResizeMode.ResizeTopLeft;
                 return;
             }
 
             // Bottom left corner
-            if(e.Location.Y > Height - ResizeBorderSize) {
+            if (BottomLeftResize && e.Location.Y > Height - ResizeBorderSize) {
                 Cursor = Cursors.SizeNESW;
                 _resizeMode = ResizeMode.ResizeBottomLeft;
                 return;
             }
 
-            // Left edge
-            Cursor = Cursors.SizeWE;
-            _resizeMode = ResizeMode.ResizeLeft;
-            return;
+            if (LeftResize) { 
+                // Left edge
+                Cursor = Cursors.SizeWE;
+                _resizeMode = ResizeMode.ResizeLeft;
+                return;
+            }
         }
 
         if (e.Location.X > Width - ResizeBorderSize) {
             // Top right corner
-            if(e.Location.Y < ResizeBorderSize) {
+            if(TopRightResize && e.Location.Y < ResizeBorderSize) {
                 Cursor = Cursors.SizeNESW;
                 _resizeMode = ResizeMode.ResizeTopRight;
                 return;
             }
 
             // Bottom right corner
-            if (e.Location.Y > Height - ResizeBorderSize) {
+            if (BottomLeftResize && e.Location.Y > Height - ResizeBorderSize) {
                 Cursor = Cursors.SizeNWSE;
                 _resizeMode = ResizeMode.ResizeBottomRight;
                 return;
             }
         }
 
-        if (e.Location.Y > Height - ResizeBorderSize) {
+        if (BottomResize && e.Location.Y > Height - ResizeBorderSize) {
             // Bottom edge
             Cursor = Cursors.SizeNS;
             _resizeMode = ResizeMode.ResizeBottom;
             return;
         }
 
-        if(e.Location.Y < ResizeBorderSize) {
+        if(TopResize && e.Location.Y < ResizeBorderSize) {
             // Top edge
             Cursor = Cursors.SizeNS;
             _resizeMode = ResizeMode.ResizeTop;
             return;
         }
 
-        if(e.Location.X > Width - ResizeBorderSize) {
+        if(RightResize && e.Location.X > Width - ResizeBorderSize) {
             // Right edge
             Cursor = Cursors.SizeWE;
             _resizeMode = ResizeMode.ResizeRight;
@@ -184,4 +183,38 @@ internal class ResizablePanel : Panel {
         base.OnPaint(e);
     }
 
+}
+
+
+internal partial class ResizablePanel {
+    [Description("Resize border size")]
+    public int ResizeBorderSize { get; set; } = 10;
+    [Description("Default Cursor")]
+    public Cursor NormalCursor { get; set; } = Cursors.Default;
+    [Description("Border Colour")]
+    public Color BorderColour { get; set; } = Color.Black;
+    [Description("Border Style")]
+    public ButtonBorderStyle RBorderStyle { get; set; } = ButtonBorderStyle.Solid;
+    [Description("Border Width")]
+    public int BorderWidth { get; set; } = 1;
+    [Description("Keep Aspect")]
+    public bool KeepAspectCornerOnly { get; set; } = false;
+    public Size Aspect { get; set; } = new Size(16, 9);
+
+    [Category("Resize Flags")]
+    public bool TopLeftResize { get; set; } = true;
+    [Category("Resize Flags")]
+    public bool TopResize { get; set; } = true;
+    [Category("Resize Flags")]
+    public bool TopRightResize { get; set; } = true;
+    [Category("Resize Flags")]
+    public bool RightResize { get; set; } = true;
+    [Category("Resize Flags")]
+    public bool BottomRightResize { get; set; } = true;
+    [Category("Resize Flags")]
+    public bool BottomResize { get; set; } = true;
+    [Category("Resize Flags")]
+    public bool BottomLeftResize { get; set; } = true;
+    [Category("Resize Flags")]
+    public bool LeftResize { get; set; } = true;
 }
